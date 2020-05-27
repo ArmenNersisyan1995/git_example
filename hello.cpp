7f6ac598d22760cbc853e2dcc55ee8b9775c5064
ArmenNersisyan1995 <armennersesyan95@gmail.com>
2020-05-27 05:45:33 +0400

    Revert "Added function "second" to file "hello.cpp""
    This reverts commit 4161103a0972cebb6c2539f6d6db1f25aac0555a.

--- hello.cpp
+++ hello.cpp
@@ -1,13 +1,8 @@
 #include<iostream>
 
-void second(void){
-    std::cout<<"Second Commit"<<std::endl;
-}
-
 int main(){
 
     std::cout<<"Hello World"<<std::endl;
-	second();
 }
 ~                                                                               
 ~                                                                               
