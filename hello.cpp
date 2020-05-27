116505853e69b7463259ce015f1e5dbfa1dc7410
ArmenNersisyan1995 <armennersesyan95@gmail.com>
2020-05-27 05:51:26 +0400

    Added function "first" to file "hello.cpp"

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
-
+void first(void){
+	std::cout<<"first branch"<<std::endl;
+}
 int main(){
 
     std::cout<<"Hello World"<<std::endl;
+	first();
 }
 ~                                                                               
 ~                                                                               
