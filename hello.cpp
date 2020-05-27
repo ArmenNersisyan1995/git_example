4161103a0972cebb6c2539f6d6db1f25aac0555a
ArmenNersisyan1995 <armennersesyan95@gmail.com>
2020-05-27 05:33:27 +0400

    Added function "second" to file "hello.cpp"

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
 
+void second(void){
+    std::cout<<"Second Commit"<<std::endl;
+}
+
 int main(){
 
     std::cout<<"Hello World"<<std::endl;
+	second();
 }
 ~                                                                               
 ~                                                                               
